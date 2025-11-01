d9acb4384b7077bcc3a28bb60795ee2c3677f974
washkinazy <tech@ashkinazy.net>
2025-11-01 16:12:59 -0500

    lots of small changes

---
+++ files/system/usr/share/cerulean/just/colors.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# ANSI color codes for use in just recipes and scripts
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'  # No Color
